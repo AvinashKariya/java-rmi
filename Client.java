f261f77dd63157ecfdddabfdad906f6fc9e0ab98
Avinash Kariya <avinashkariya05910@gmail.com>
2023-04-18 19:08:43 +0530

    First Commit

---
+++ Client.java
@@ -0,0 +1,9 @@
+import java.rmi.Naming;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        AddI obj = (AddI) Naming.lookup("ADD");
+        int n = obj.add(5, 4);
+        System.out.println("addition" + n);
+    }
+}
